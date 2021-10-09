65ce6536d1349fa7459c0b8b92e3b9c1a2d75681
Francisco Ovares Rojas <fran1709@estudiantec.cr>
2021-10-08 22:00:45 -0600

    Images

--- GestorDePlanEstudios/src/com/sistema/gui/MainGui.java
+++ GestorDePlanEstudios/src/com/sistema/gui/MainGui.java
@@ -1,2 +1,49 @@
-package com.sistema.gui;public class mainGui {
+package com.sistema.gui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainGui extends JFrame{
+
+    private JPanel Window;
+    private JButton registrosButton;
+    private JButton salirButton;
+    private JButton consultasButton;
+
+    public MainGui() {
+        setContentPane(Window);
+        setTitle("Sistema Gestor de Planes de Estudio");
+        setSize(450,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setLocationRelativeTo(null);
+
+        salirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        consultasButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                Consultas nuevaConsulta = new Consultas();
+                nuevaConsulta.setVisible(true);
+            }
+        });
+        registrosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                registros nuevoRegistros = new registros();
+                nuevoRegistros.setVisible(true);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        MainGui guiPrincipal = new MainGui();
+    }
 }
--- GestorDePlanEstudios/src/com/sistema/gui/registros.java
+++ GestorDePlanEstudios/src/com/sistema/gui/registros.java
@@ -1,2 +1,33 @@
-package com.sistema.gui;public class registros {
+package com.sistema.gui;
+
+import com.sistema.Main;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class registros extends JFrame {
+    private JButton registrarEscuelaOÁreaButton;
+    private JPanel WindowRegistrar;
+    private JButton registrarPlanesDeEstudioButton;
+    private JButton registrarCursosButton;
+    private JButton asignarRequisitosYCorrequisitosButton;
+    private JButton volverButton;
+
+    public registros() {
+        setContentPane(WindowRegistrar);
+        setTitle("Sistema Gestor de Planes de Estudio");
+        setSize(450,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        volverButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                MainGui nuevaMainGui = new MainGui();
+                nuevaMainGui.setVisible(true);
+            }
+        });
+    }
 }
